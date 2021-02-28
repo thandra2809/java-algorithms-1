6af88ebe2e18c00abe30eb82cfa15a4c588b0130
Srinivasa Rao THANDRA <thandra2809>
2021-02-27 20:35:10 -0800

    Testing

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Testing this for the first time in eclipse after 10 years1");
+		System.out.println("Testing this for the first time in eclipse after 10 years-with Git");
 	}
 
 }
